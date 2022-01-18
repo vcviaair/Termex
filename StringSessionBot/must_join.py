f8c5d74c24374769d0d938a2894f14194f867371
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-18 13:22:57 -0800

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -19,14 +19,14 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"يجب ان تشترك [جيبثون العرب]({https://t.me/Jepthon}) حتى تتمكن من استخدامي.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton("✨ يرجى الاشتراك في القناة ✨", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"انا لست ادمن في المجموعة MUST_JOIN  : {MUST_JOIN} !")
